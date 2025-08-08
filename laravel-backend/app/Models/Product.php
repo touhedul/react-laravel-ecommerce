319ccfcbcfe1c8ba2a96164613a99d7867ed4668
MD Touhedul Islam <110459773+ratul-prolific@users.noreply.github.com>
2025-08-08 15:43:58 +0600

    Product CRUD WIP

---
+++ laravel-backend/app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+
+        'title',
+        'description',
+        'price',
+        'compare_price',
+        'quantity',
+        'category_id',
+        'status',
+        'is_featured',
+        'image',
+        'sku',
+    ];
+}
---
+++ laravel-backend/app/Models/ProductSize.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSize extends Model
+{
+    //
+    protected $fillable = ['product_id', 'size_id'];
+}
